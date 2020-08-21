a692b6cd67fb4298b2ac957f53250452b5c2fc88
Willem A Duiker <duikhead2@gmail.com>
2020-08-21 16:29:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,11 @@ end
 def using_flatten(array)
   array.flatten()
 end
+
+def using_delete(array,element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
